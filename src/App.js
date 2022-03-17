cad6e0ca54965d3cb08ebe04c44b4367b9315014
sharukhk08 <iamsharukh08@gmail.com>
2022-03-17 15:06:06 +0530

    set dark mode and light mode with localstorage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
 import AppRoutes from "./AppRoutes";
-import { useState } from "react";
-import {
-  MantineProvider,
-  ColorSchemeProvider,
-  ColorScheme,
-} from "@mantine/core";
+import { useEffect, useState } from "react";
+import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
 
 const myTheme = {
   colorScheme: "light",
@@ -14,15 +11,63 @@ const myTheme = {
 };
 
 function App() {
-  const [colorScheme, setColorScheme] = useState(myTheme.colorScheme);
-  const toggleColorScheme = (value) =>
-    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+  // const [colorScheme, setColorScheme] = useLocalStorage({
+  //   key: "mantine-color-scheme",
+  //   defaultValue: "light",
+  // });
+
+  const [colorScheme, setColorScheme] = useState(myTheme);
+  const toggleColorScheme = () => {
+    console.log(colorScheme.colorScheme);
+    //  check if color scheme is light or dark
+    if (colorScheme.colorScheme === "light") {
+      setColorScheme({
+        ...colorScheme,
+        colorScheme: "dark",
+      });
+      setValue("dark");
+    }
+    if (colorScheme.colorScheme === "dark") {
+      setColorScheme({
+        ...colorScheme,
+        colorScheme: "light",
+      });
+      setValue("light");
+    }
+  };
+  const [value, setValue] = useLocalStorage({
+    key: "color-scheme",
+    defaultValue: colorScheme.colorScheme,
+  });
+
+  useEffect(() => {
+    if (value === "light") {
+      setColorScheme({
+        ...colorScheme,
+        colorScheme: "light",
+      });
+    }
+    if (value === "dark") {
+      setColorScheme({
+        ...colorScheme,
+        colorScheme: "dark",
+      });
+    }
+  }, []);
+
+  // useHotkeys("ctrl+shift+c", toggleColorScheme);
+  console.log(colorScheme, "colorScheme");
+  console.log(myTheme, "myTheme");
+  console.log(value, "value");
+
+  // useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
   return (
     <ColorSchemeProvider
-      colorScheme={colorScheme}
+      colorScheme={colorScheme.colorScheme}
       toggleColorScheme={toggleColorScheme}
     >
-      <MantineProvider theme={{ colorScheme }}>
+      <MantineProvider theme={colorScheme}>
         <AppRoutes />
       </MantineProvider>
     </ColorSchemeProvider>
