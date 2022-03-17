921e5d3b79de54675e32aa8e0f34e63a37dc465c
sharukhk08 <iamsharukh08@gmail.com>
2022-03-17 16:43:34 +0530

    add not route found

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -4,6 +4,7 @@ import SignUp from "./views/SignUp";
 import Home from "./views/Home";
 import ToadayTask from "./components/ToadayTask";
 import AddTask from "./components/AddTask";
+import NoRouteFound from "./components/common/NoRouteFound";
 
 const AppRoutes = () => {
   return (
@@ -13,9 +14,11 @@ const AppRoutes = () => {
           <Route path="dashboard" element={<Home />}>
             <Route path="add-task" element={<AddTask />} />
             <Route path="today-task" element={<ToadayTask />} />
+            <Route path="/dashboard" element={<NoRouteFound />} />
           </Route>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<NoRouteFound />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/common/NoRouteFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NoRouteFound = () => {
+  return <div>NoRouteFound</div>;
+};
+
+export default NoRouteFound;
